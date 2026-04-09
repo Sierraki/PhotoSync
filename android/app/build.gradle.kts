abed7672e6c5477d8faa0dde7ce03c6296723e56
Sierraki <sierrakiper@outlook.com>
2026-04-10 06:13:59 +0800

    20260410

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.photosync.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.5"
+        versionCode = 8
+        versionName = "1.6"
     }
 
     buildTypes {
